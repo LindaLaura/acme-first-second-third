34cf152e261cd880710de82ae78b72394ef71495
Linda Nzeukang <lindalauranzeukang@gmail.com>
2021-02-04 08:58:58 -0600

    second commit

--- myScript.js
+++ myScript.js
@@ -7,5 +7,25 @@ const users = [
     {id:4, name: 'lucy', slot: 'third', selected : true},
 ];
 
-let firstBox = document.getElementById('first');
-alert(document.getElementById('first'));
+const listBtn = document.querySelectorAll("button");
+const btnsArr = Array.from(listBtn);
+console.log(btnsArr);
+const user1 = document.getElementsByClassName("id1");
+console.log(user1);
+
+const next1 = document.getElementsByClassName("next1");
+console.log(next1);
+//next1[0].addEventListener("click", changeSlot)
+user1[0].addEventListener("click", changeColor);
+
+function changeColor(ev)
+{
+    let target = ev.target;
+    if (target.style.backgroundColor === 'orangered'){
+        target.style.backgroundColor = 'white'
+    }
+    else
+    {
+        target.style.backgroundColor = 'orangered';
+    }
+}
